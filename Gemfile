705f765adad28efa0296cdd142147c15f3f36794
Dana Sevcik <danasevcik@hotmail.com>
2019-04-26 17:01:55 -0400

    secret key

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ gem "jwt", "~> 2.1"
 gem "active_model_serializers", "~> 0.10.9"
 
 gem "faker", "~> 1.9"
+gem 'dotenv-rails', groups: [:development, :test]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
 
   def encode_token(payload)
     # should store secret in env variable
-    JWT.encode(payload, 'my_s3cr3t')
+    JWT.encode(payload, ENV['SECRET_KEY'])
   end
 
   def auth_header
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::API
       token = auth_header.split(' ')[1]
       # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+        JWT.decode(token, ENV['SECRET_KEY'], true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
