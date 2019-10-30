19da63230a0bf890041739485d7e6b5204e78e43
Dana Sevcik <danasevcik@hotmail.com>
2019-10-30 13:25:38 -0400

    adds comments

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -23,6 +23,7 @@ class Api::V1::MessagesController < ApplicationController
 
   def mark_as_read
     # mark seen messages as read (called once a user opens a chat)
+    # iterate through the messages and check unread message attribute on the obj
     @user = User.find(params[:user][:id])
     @conversation = Conversation.find(params[:conversation][:id])
     @messages = @conversation.messages
